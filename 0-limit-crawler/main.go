7e58628f35368626da6f7d06ce5e5014120b5a68
Eriks Vitolins <eriks.vitolins@testdevlab.com>
2021-08-11 16:43:24 +0300

    finishing task one

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,17 +12,19 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, limmiter <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
 
+	<-limmiter
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
@@ -30,20 +32,20 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	}
 
 	fmt.Printf("found: %s %q\n", url, body)
-
 	wg.Add(len(urls))
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, limmiter)
 	}
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
+	limitter := time.Tick(time.Second)
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, limitter)
 	wg.Wait()
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Weeping-Willow/go-concurrency-exercises
+
+go 1.16
